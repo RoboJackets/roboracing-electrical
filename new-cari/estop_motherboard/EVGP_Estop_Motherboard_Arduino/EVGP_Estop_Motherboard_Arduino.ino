2b599f22c298a9ff7d8038ad92ec02fac90176c4
Josiah-Koh <56281027+Josiah-Koh@users.noreply.github.com>
2020-01-20 18:00:49 -0500

    removed arduino folder

--- new-cari/estop_motherboard/EVGP_Estop_Motherboard_Arduino/EVGP_Estop_Motherboard_Arduino.ino
+++ new-cari/estop_motherboard/EVGP_Estop_Motherboard_Arduino/EVGP_Estop_Motherboard_Arduino.ino
@@ -1,9 +1,19 @@
+#define radioSafe 18
+#define radioStart 19
+
+#define driveIn 21
+#define steeringIn 20
+#define driveOut 29
+#define steeringOut 30
+
+#define ETH_RST 13
+
 void setup() {
-  // put your setup code here, to run once:
+
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
 
 }
